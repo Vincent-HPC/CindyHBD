62d23ddb720ad14d7d83b8eb7fbffa3583171ddb
Vincent <pchuang92738@gmail.com>
2018-09-10 22:36:40 +0800

    two step play music

--- effect.js
+++ effect.js
@@ -35,50 +35,30 @@ $('document').ready(function () {
 		$('#bulb_orange').addClass('bulb-glow-orange');
 		$('body').addClass('peach');
 		$(this).fadeOut('slow').delay(500).promise().done(function () {
-			// $('#play').click();
-			var audio = $('.song')[0];
-			audio.play();
-			audio.pause();
-			audio.play();
-			// $('.song').prop("volume", 0.5);
-			$('.song').prop("volume", 1);
-			$('#bulb_yellow').addClass('bulb-glow-yellow-after');
-			$('#bulb_red').addClass('bulb-glow-red-after');
-			$('#bulb_blue').addClass('bulb-glow-blue-after');
-			$('#bulb_green').addClass('bulb-glow-green-after');
-			$('#bulb_pink').addClass('bulb-glow-pink-after');
-			$('#bulb_orange').addClass('bulb-glow-orange-after');
-			$('body').css('backgroud-color', '#FFF');
-			$('body').addClass('peach-after');
-			// $(this).fadeOut('slow').delay(6000).promise().done(function () {
-			// 	$('#bannar_coming').fadeIn('slow');
-			// });
-			$(this).delay(16000).promise().done(function () {
-				$('#bannar_coming').click();
-			});
+			$('#play').fadeIn('slow');
 
 		});
 	});
-	// $('#play').click(function () {
-	// 	var audio = $('.song')[0];
-	// 	audio.play();
-	// 	// $('.song').prop("volume", 0.5);
-	// 	$('.song').prop("volume", 1);
-	// 	$('#bulb_yellow').addClass('bulb-glow-yellow-after');
-	// 	$('#bulb_red').addClass('bulb-glow-red-after');
-	// 	$('#bulb_blue').addClass('bulb-glow-blue-after');
-	// 	$('#bulb_green').addClass('bulb-glow-green-after');
-	// 	$('#bulb_pink').addClass('bulb-glow-pink-after');
-	// 	$('#bulb_orange').addClass('bulb-glow-orange-after');
-	// 	$('body').css('backgroud-color', '#FFF');
-	// 	$('body').addClass('peach-after');
-	// 	// $(this).fadeOut('slow').delay(6000).promise().done(function () {
-	// 	// 	$('#bannar_coming').fadeIn('slow');
-	// 	// });
-	// 	$(this).delay(16000).promise().done(function () {
-	// 		$('#bannar_coming').click();
-	// 	});
-	// });
+	$('#play').click(function () {
+		var audio = $('.song')[0];
+		audio.play();
+		// $('.song').prop("volume", 0.5);
+		$('.song').prop("volume", 1);
+		$('#bulb_yellow').addClass('bulb-glow-yellow-after');
+		$('#bulb_red').addClass('bulb-glow-red-after');
+		$('#bulb_blue').addClass('bulb-glow-blue-after');
+		$('#bulb_green').addClass('bulb-glow-green-after');
+		$('#bulb_pink').addClass('bulb-glow-pink-after');
+		$('#bulb_orange').addClass('bulb-glow-orange-after');
+		$('body').css('backgroud-color', '#FFF');
+		$('body').addClass('peach-after');
+		// $(this).fadeOut('slow').delay(6000).promise().done(function () {
+		// 	$('#bannar_coming').fadeIn('slow');
+		// });
+		$(this).fadeOut('slow').delay(16000).promise().done(function () {
+			$('#bannar_coming').click();
+		});
+	});
 
 	// Let's Decorate
 	$('#bannar_coming').click(function () {
